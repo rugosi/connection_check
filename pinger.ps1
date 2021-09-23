b65981a4dad0f56d89db0c425712d0ed3cd3b8d7
rugosi <kukacster@gmail.com>
2021-09-23 19:59:44 +0200

    Adding pinger.ps1

---
+++ pinger.ps1
@@ -0,0 +1,73 @@
+$badconnection = $false
+$starttime = $none
+$endtime = $none
+$counter = 0
+$firstrun = Get-Date -Format "yyyy.MM.dd_HH:mm:ss"
+$firstrun = "connection.log." + $firstrun
+$1sec = 10000000
+$debug = $false
+
+write-host -ForegroundColor yellow "Press any key to quit"
+write-host "-----------------"
+
+
+function Print-Connection-Status {
+  [CmdletBinding()]
+  param (
+    [string]$ConnResult
+  )
+  write-host -NoNewLine ($counter).ToString('00000000') "connection "
+  if ($ConnResult -like "OK")
+  {
+    write-host -ForegroundColor green $ConnResult
+  } elseif ($ConnResult -like "NOK") {
+    write-host -ForegroundColor red $ConnResult
+  }
+}
+
+while ($true) {
+
+  $result = (Test-Connection -IPv4 -TargetName "index.hu" -Count 1 -TimeoutSeconds 1 -Quiet)
+  $counter++
+
+#  DEBUG
+  if ($debug -eq $true -and $counter -gt 2 -and $counter -lt 6)
+  {
+    $result = $false
+  }
+
+
+  if ($result -eq $false)
+  {
+    if ($badconnection -eq $false)
+    {
+        $starttime = Get-Date -Format "yyyy.MM.dd HH:mm:ss"
+        $badconnection = $true
+    }
+    $endtime = Get-Date -Format "yyyy.MM.dd HH:mm:ss"
+
+    Print-Connection-Status -ConnResult "NOK"
+
+  } else {
+    if ($badconnection -eq $true)
+      {
+        $begin = Get-Date -Date $starttime
+        $end = Get-Date -Date $endtime
+        $diff = $end - $begin + $1sec
+        write-host -ForegroundColor yellow "In degraded mode: " $starttime " -> " $endtime " = " $diff
+        write-output $starttime";"$endtime";"$diff >> .\$firstrun
+        $badconnection = $false
+      }
+
+     Print-Connection-Status -ConnResult "OK"
+  }
+
+  Start-Sleep -Seconds 1
+
+  if ([console]::KeyAvailable)
+  {
+    write-host -ForegroundColor yellow "Key pressed. End."
+    write-host
+    break
+  }
+}
